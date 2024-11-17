e8adb4c462e42b8a56ac06d86a35119215d2f420
‘Thashmila <thashmilajayasinghe@gmail.com>
2024-11-17 14:01:57 +0530

    redundant states removes and code cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,15 @@
 import { useState, useEffect, useRef } from 'react';
 import tomatoLogo from '/logo.svg';
-import settingsIcon from '/settings.svg';
-import Modal from './Modal.jsx'
+
+const FOCUS_TIME = 25;
+const SHORT_BREAK_TIME = 5;
+const LONG_BREAK_TIME = 20;
 
 function App() {
   const [status, setStatus] = useState("focus");
-  const [timeLeft, setTimeLeft] = useState(25 * 60);
+  const [timeLeft, setTimeLeft] = useState(FOCUS_TIME * 60);
   const [sessionsLeft, setSessionsLeft] = useState(4);
   const [isRunning, setIsRunning] = useState(false);
-  const [completedSessions, setCompletedSessions] = useState(0);
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const [focusTime, setFocusTime] = useState(25);
-  const [shortBreakTime, setShortBreakTime] = useState(5);
-  const [longBreakTime, setLongBreakTime] = useState(20);
 
   const timerRef = useRef(null);
   const bellRef = useRef(null);
@@ -59,23 +55,23 @@ function App() {
     if (status === "focus") {
       if (timeLeft === 0) {
         // focus session ends without skips
-        setCompletedSessions((prev) => prev + 1);
+        // setCompletedSessions((prev) => prev + 1);
         setSessionsLeft((prev) => prev - 1);
   
         if (sessionsLeft > 1) {
           setStatus("short");
-          setTimeLeft(5 * 60); // short break duration
+          setTimeLeft(SHORT_BREAK_TIME * 60); // short break duration
         } else {
           setStatus("long");
-          setTimeLeft(20 * 60); // long break duration
+          setTimeLeft(LONG_BREAK_TIME * 60); // long break duration
           setSessionsLeft(4); // resetting the cycle
-          setCompletedSessions(0); // resetting completed sessions
+          // setCompletedSessions(0); // resetting completed sessions
         }
       }
     } else if (status === "short" || status === "long") {
       // switching to a new focus session after a break
       setStatus("focus");
-      setTimeLeft(25 * 60); // focus session duration
+      setTimeLeft(FOCUS_TIME * 60); // focus session duration
     }
   };
 
@@ -90,41 +86,29 @@ function App() {
     if (status === "focus") {
       // skipping to a short break without decrementing sessionsLeft
       setStatus("short");
-      setTimeLeft(5 * 60); // Short break duration
+      setTimeLeft(SHORT_BREAK_TIME * 60); // Short break duration
     } else {
       // switching to focus session after breaks
       setStatus("focus");
-      setTimeLeft(25 * 60); // focus session duration
+      setTimeLeft(FOCUS_TIME * 60); // focus session duration
     }
   };
 
   const handleReset = () => {
     clearInterval(timerRef.current);
     setStatus("focus");
-    setTimeLeft(focusTime * 60);
+    setTimeLeft(FOCUS_TIME * 60);
     setSessionsLeft(4);
     setIsRunning(false);
-  };
-
-  const handleSaveSettings = () => {
-    setTimeLeft(focusTime * 60); // updating timer with new focus time
-    setIsModalOpen(false);
-    handleReset(); // resetting the app with new settings
   };
 
   return (
     <div>
       <div id="header-section">
-
         <div className="heading">
-          <img src={tomatoLogo} className="logo" alt="settings icon" />
+          <img src={tomatoLogo} className="logo" alt="logo" />
           <h1>Pomodoro Timer</h1> 
         </div>
-
-        <button onClick={() => setIsModalOpen(true)} id="settings-btn">
-          <img src={settingsIcon} className="icon" alt="settings icon" />
-        </button>
-
       </div>
 
       <div id="body-section">
@@ -144,7 +128,7 @@ function App() {
             ).padStart(2, "0")}`
           }
           </p>
-          <p id="session-label" >{`focus sessions left till long break: ${sessionsLeft}`}</p>
+          <p id="session-label" >{`Focus sessions left till next long break: ${sessionsLeft}`}</p>
           <button 
             id="start_stop" 
             className={`${status}1 buttons btn`}
@@ -172,60 +156,6 @@ function App() {
         </div>
 
       </div>
-
-      <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
-        <h2>Settings</h2>        
-        <p>Time durations are in minutes</p>
-        <div className="settings-form">
-          
-          <label htmlFor="focus-time" className="settings-label">Pomodoro Length :</label>
-          <input 
-            id="focus-time" 
-            type="number" 
-            className="settings-input" 
-            value={focusTime}
-            onChange={(e) => setFocusTime(+e.target.value)}
-            min={1}
-          />
-
-          <label htmlFor="short-break" className="settings-label">Short Break Length :</label>
-          <input 
-            id="short-break" 
-            type="number" 
-            className="settings-input" 
-            value={shortBreakTime}
-            onChange={(e) => setShortBreakTime(+e.target.value)}
-            min={1}
-          />
-
-          <label htmlFor="long-break" className="settings-label">Long Break Length :</label>
-          <input 
-            id="long-break" 
-            type="number" 
-            className="settings-input" 
-            value={longBreakTime}
-            onChange={(e) => setLongBreakTime(+e.target.value)}
-            min={1}
-          />
-
-          <label htmlFor="cycles" className="settings-label">Number of Pomodoros per Cycle :</label>
-          <input 
-            id="cycles" 
-            type="number" 
-            className="settings-input" 
-            value={sessionsLeft}
-            onChange={(e) => setSessionsLeft(+e.target.value)}
-            min={1}
-          />
-        
-        </div>
-        <button className="modal-btns btn" onClick={handleSaveSettings}>
-          save
-        </button>
-        <button className="modal-btns btn" onClick={handleReset}>
-          reset
-        </button>
-      </Modal>
 
     </div>
   );
